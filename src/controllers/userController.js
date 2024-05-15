7b2e7c82519373dc74fd713d350f37a3e1e14e20
Vivek Kumar <vivek@culturtap.com>
2024-05-15 20:45:38 +0530

    updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,7 @@ export const getUser = async (req, res) => {
             return res.status(200).json(user);
         }
         else {
-            return res.status(404).json({ message: 'User not found' });
+            return res.json({ status: 404, message: 'User not found' });
         }
     } catch (error) {
         res.status(500);
