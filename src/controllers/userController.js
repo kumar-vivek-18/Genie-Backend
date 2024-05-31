2670a6b1b9a0df70402f249144bc76faf7f03b41
Vivek Kumar <vivek@culturtap.com>
2024-05-31 17:42:10 +0530

    updated create spade flow

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,7 +47,7 @@ export const createRequest = async (req, res) => {
 
         const uniqueTokens = [];
 
-        if (!retailers || retailers.length) {
+        if (!retailers || !retailers.length) {
             return res.status(404).json({ message: 'No retailers found for the requested category' });
         }
 
