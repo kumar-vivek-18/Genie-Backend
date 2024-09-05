1c511cb106577ce19632f19ecf3b95467eb84d2a
Vivek Kumar <vivek@culturtap.com>
2024-09-05 22:28:23 +0530

    updated version

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -485,7 +485,7 @@ export const updatePaymentStatus = async (req, res) => {
 
 export const currentVersion = async (req, res) => {
     try {
-        const version = "1.14.0";
+        const version = "1.15.0";
         return res.status(200).json(version);
     } catch (error) {
         return res.status(500).json({ message: "Internal Server Error" });
