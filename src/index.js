5239f6120ab838714d16ce720679935d0aaf989a
Vivek Kumar <vivek@culturtap.com>
2024-09-30 17:40:24 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -329,7 +329,7 @@ io.on("connection", (socket) => {
 // Connect to the database and start the server
 connectDB()
     .then(() => {
-        server.listen(5000, '0.0.0.0', () => {
+        server.listen(process.env.PORT, '0.0.0.0', () => {
             console.log('Server is running on port 5000');
         });
 
