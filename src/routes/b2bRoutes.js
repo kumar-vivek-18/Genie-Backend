1bc82aa063f9025f81bfb29b7a7334bbf8c71c46
Vivek Kumar <vivek@culturtap.com>
2024-11-28 00:06:38 +0530

    fixed commit error

--- src/routes/b2bRoutes.js
+++ src/routes/b2bRoutes.js
@@ -21,5 +21,4 @@ router.route('/remove-productimg').delete(removeProductImage)
 router.route('/get-all-categories').get(availableCategories)
 router.route('/delete-request').delete(deleteUserRequest)
 
-
 export default router;
